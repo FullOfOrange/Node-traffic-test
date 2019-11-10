fc53a74a8848cf089ee3da03c1f4e23db56787cf
JinHyoukYork <jdd04026@gmail.com>
2019-11-10 23:47:36 +0900

    feat: 0st testing, test redis transaction
    redis의 transaction 을 테스팅해봄.

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
-    
+const REDIS_OPTIONS = {
+    host: "127.0.0.1",
+    port: 6379
+}
+//initRedis로 남은 티켓 장수를 설정, key: "0"
+const redis = require('redis'),
+      client = redis.createClient(REDIS_OPTIONS);
+
+app.get('/', checkTicketLeft, (req,res) => {
+    res.send()
 });
 
+async function checkTicketLeft(req,res,next){
+    date = new Date()
+    multi = client.multi();
+    multi.mget("0",(err,rep) => {
+        if(+rep[0] < 0){
+            console.log('a');
+            multi.decr("0",redis.print);
+        }
+    })
+    multi.exec((err,rep) => {
+        console.log(date,rep)
+        if(rep < 0) {
+            res.status(404).send()
+        } else {
+            next()
+        }
+    })
+}
 app.listen(port);
